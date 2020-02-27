5f0646a5eec1f9bf14cac9dc4681abfbb54cb9a5
marion henrio <marion.henrio@hotmail.fr>
2020-02-27 16:54:41 +0100

    on track

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doses', to: 'doses#_index'
+# get 'cocktails', to: 'cocktails#index'
+# get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+# post 'cocktails', to: 'cocktails#create'
+# get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+# get 'cocktails/:id/doses/new'
+# post 'cocktails/:id/doses'
+# delete 'doses/id'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses
+
+
+  end
 end
